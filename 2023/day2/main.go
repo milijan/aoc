e52e0d59a8ef336a38ffb2be68f18e9f5d22d086
milijan <milijan@users.noreply.github.com>
2023-12-02 18:35:34 +0000

    day2

---
+++ 2023/day2/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	// only consider numbers
+	puzzle1, err := os.ReadFile(filepath.Join(cwd, "puzzle.txt"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	puzzle := strings.Split(string(puzzle1), "\n")
+	sol1 := Solver1(puzzle)
+	fmt.Println("-> puzzle 1:", sol1)
+
+	// consider numbers and literals
+	puzzle2, err := os.ReadFile(filepath.Join(cwd, "puzzle.txt"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	puzzle = strings.Split(string(puzzle2), "\n")
+	sol2 := Solver2(puzzle)
+	fmt.Println("-> puzzle 2:", sol2)
+}
