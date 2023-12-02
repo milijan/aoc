9d9f1eaf432f72d609371ce15263249836fbb0c0
milijan <milijan@users.noreply.github.com>
2023-12-02 18:35:33 +0000

    part2

--- 2023/day1/solver.go
+++ 2023/day1/solver.go
@@ -5,28 +5,53 @@ import (
 	"strings"
 )
 
+func ReplaceFirst(line string, digitsStr []string) (string, int) {
+	digitsNum := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	index := -1
+	digitRank := -1
+	count := 0
+	for i, digit := range digitsStr {
+		rank := strings.Index(line, digit)
+		if rank >= 0 {
+			count++
+			if digitRank < 0 || rank < digitRank {
+				digitRank = rank
+				index = i
+			}
+		}
+	}
+	if index >= 0 {
+		line = strings.Replace(line, digitsStr[index], digitsNum[index], 1)
+	}
+	return line, count
+}
+
+func ReplaceLast(line string, digitsStr []string) string {
+	digitsNum := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	index := -1
+	digitRank := -1
+	for i, digit := range digitsStr {
+		rank := strings.LastIndex(line, digit)
+		if rank >= 0 {
+			if digitRank < 0 || rank > digitRank {
+				digitRank = rank
+				index = i
+			}
+		}
+	}
+	if index >= 0 {
+		line = strings.ReplaceAll(line, digitsStr[index], digitsNum[index])
+	}
+	return line
+}
+
 func Interpret(line string) string {
 	//line = strings.ToLower(line)
 	digitsStr := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
-	digitsNum := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
-	index := -1
-	if len(line) > 2 {
-		digitRank := -1
-		for i, digit := range digitsStr {
-			rank := strings.Index(line, digit)
-			if rank >= 0 {
-				if digitRank < 0 || rank < digitRank {
-					digitRank = rank
-					index = i
-				}
-			}
-		}
-		if index >= 0 {
-			line = strings.Replace(line, digitsStr[index], digitsNum[index], 1)
-			return Interpret(line)
-		} else {
-			return line
-		}
+
+	line, count := ReplaceFirst(line, digitsStr)
+	if count > 1 {
+		line = ReplaceLast(line, digitsStr)
 	}
 	return line
 }
