c4501af01cfb0bb944c71657b869d4353a892988
milijan <milijan@users.noreply.github.com>
2023-12-03 07:58:37 +0000

    day3

---
+++ 2023/day3/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"golang.org/x/oauth2"
+)
+
+func GetPuzzle() {
+	ctx := context.Background()
+	conf := &oauth2.Config{
+		ClientID:     "YOUR_CLIENT_ID",
+		ClientSecret: "YOUR_CLIENT_SECRET",
+		Scopes:       []string{"SCOPE1", "SCOPE2"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://github.com/login/oauth/authorize",
+			TokenURL: "https://github.com/login/oauth/access_token",
+		},
+	}
+
+	// use PKCE to protect against CSRF attacks
+	// https://www.ietf.org/archive/id/draft-ietf-oauth-security-topics-22.html#name-countermeasures-6
+	verifier := oauth2.GenerateVerifier()
+
+	// Redirect user to consent page to ask for permission
+	// for the scopes specified above.
+	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline, oauth2.S256ChallengeOption(verifier))
+	fmt.Printf("Visit the URL for the auth dialog: %v", url)
+
+	// Use the authorization code that is pushed to the redirect
+	// URL. Exchange will do the handshake to retrieve the
+	// initial access token. The HTTP Client returned by
+	// conf.Client will refresh the token as necessary.
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		panic(err)
+	}
+	tok, err := conf.Exchange(ctx, code, oauth2.VerifierOption(verifier))
+	if err != nil {
+		panic(err)
+	}
+
+	client := conf.Client(ctx, tok)
+	client.Get("...")
+}
+
+func LoadPuzzle(filename string) []string {
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	content, err := os.ReadFile(filepath.Join(cwd, filename))
+	if err != nil {
+		fmt.Println(err)
+		return []string{}
+	}
+	puzzle := strings.Split(string(content), "\n")
+
+	return puzzle
+}
+
+func main() {
+	puzzle := LoadPuzzle("puzzle.txt")
+
+	sol1 := Solver1(puzzle)
+	fmt.Println("-> puzzle 1:", sol1)
+
+	sol2 := Solver2(puzzle)
+	fmt.Println("-> puzzle 2:", sol2)
+}
