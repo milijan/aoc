0ac9f888c3532a5deafe5e93e948c0ff69ab8d13
milijan <milijan@users.noreply.github.com>
2024-01-02 18:32:10 +0000

    day14 part2

--- 2023/day14/solver.go
+++ 2023/day14/solver.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 )
 
 type Stones struct {
@@ -10,72 +11,131 @@ type Stones struct {
 }
 
 type Puzzle struct {
-	rows int
-	cols int
-	load int
-	grid [][]byte
+	rows       int
+	cols       int
+	load       int
+	grid       [][]byte
+	emptyGrid  [][]byte
+	fixedNorth []Stones
+	fixedWest  []Stones
+	fixedSouth []Stones
+	fixedEast  []Stones
 }
 
-func FilterStones(stones []Stones, sortbyX bool, i int) []Stones {
+func FilterStones(stones *[]Stones, sortbyX bool, i int) *[]Stones {
 	filtered := []Stones{}
-	for _, s := range stones {
+	for _, s := range *stones {
 		if sortbyX && i == s.x {
 			filtered = append(filtered, s)
 		} else if i == s.y {
 			filtered = append(filtered, s)
 		}
 	}
-	return filtered
+	return &filtered
 }
 
-func SortStones(stones []Stones, sortbyX bool) []Stones {
-	for i := 0; i < len(stones); i++ {
-		for j := i + 1; j < len(stones); j++ {
-			if sortbyX && stones[i].x > stones[j].x {
-				stones[i], stones[j] = stones[j], stones[i]
-			} else if stones[i].y > stones[j].y {
-				stones[i], stones[j] = stones[j], stones[i]
+func SortStones(stones []Stones, XFirst bool, ascend bool) []Stones {
+	s := stones
+	// first axis always asending
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j < len(s); j++ {
+			if XFirst {
+				if s[i].x > s[j].x {
+					s[i], s[j] = s[j], s[i]
+				}
+			} else {
+				if s[i].y > s[j].y {
+					s[i], s[j] = s[j], s[i]
+				}
+			}
+
+		}
+	}
+	// 2nd axis respecting order
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j < len(s); j++ {
+			if XFirst { // X First
+				if s[i].x == s[j].x {
+					if ascend {
+						if s[i].y > s[j].y {
+							s[i], s[j] = s[j], s[i]
+						}
+					} else {
+						if s[i].y < s[j].y {
+							s[i], s[j] = s[j], s[i]
+						}
+					}
+				} else {
+					break
+				}
+			} else { // Y First
+				if s[i].y == s[j].y {
+					if ascend {
+						if s[i].x > s[j].x {
+							s[i], s[j] = s[j], s[i]
+						}
+					} else {
+						if s[i].x < s[j].x {
+							s[i], s[j] = s[j], s[i]
+						}
+					}
+				} else {
+					break
+				}
 			}
 		}
 	}
 	return stones
 }
 
-func FindStones(puzzle Puzzle, stone byte) []Stones {
+func FindStones(grid [][]byte, stone byte) []Stones {
 	stones := []Stones{}
-	for i, row := range puzzle.grid {
+	for i, row := range grid {
 		for j, c := range row {
 			if c == stone {
 				stones = append(stones, Stones{x: i, y: j})
 			}
 		}
 	}
-	return SortStones(stones, true)
+	return stones
 }
 
-func Rotate(puzzle Puzzle, clockwise bool) Puzzle {
-	newPuzzle := Puzzle{
-		rows: puzzle.cols,
-		cols: puzzle.rows,
-		load: puzzle.load,
+func RotateStones(stones []Stones, rows int, cols int, clockwise bool) {
+	for i := 0; i < len(stones); i++ {
+		temp := stones[i].x
+		if clockwise {
+			stones[i].x = stones[i].y
+			stones[i].y = cols - temp - 1
+		} else {
+			stones[i].x = rows - stones[i].y - 1
+			stones[i].y = temp
+		}
 	}
-	newPuzzle.grid = make([][]byte, newPuzzle.rows)
-	for i := range newPuzzle.grid {
-		newPuzzle.grid[i] = make([]byte, newPuzzle.cols)
-	}
-	for i := 0; i < newPuzzle.rows; i++ {
-		for j := 0; j < newPuzzle.cols; j++ {
-			if clockwise {
-				newPuzzle.grid[i][j] = puzzle.grid[puzzle.rows-j-1][i]
-			} else {
-				newPuzzle.grid[i][j] = puzzle.grid[j][puzzle.cols-i-1]
+}
+
+func RotateGrid(grid [][]byte, clockwise bool) [][]byte {
+	rows := len(grid)
+	rotatedGrid := [][]byte{}
+	if rows > 0 {
+		cols := len(grid[0])
+		rotatedGrid = make([][]byte, rows)
+		for i := range grid {
+			rotatedGrid[i] = make([]byte, cols)
+		}
+		for i := 0; i < rows; i++ {
+			for j := 0; j < cols; j++ {
+				if clockwise {
+					rotatedGrid[i][j] = grid[rows-j-1][i]
+				} else {
+					rotatedGrid[i][j] = grid[j][cols-i-1]
+				}
 			}
 		}
 	}
-	return newPuzzle
+	return rotatedGrid
 }
 
-func Transpose(puzzle Puzzle) Puzzle {
+func Transpose(puzzle *Puzzle) Puzzle {
 	newPuzzle := Puzzle{
 		rows: puzzle.cols,
 		cols: puzzle.rows,
@@ -93,56 +153,126 @@ func Transpose(puzzle Puzzle) Puzzle {
 	return newPuzzle
 }
 
-func MoveStones(tiltedStones []Stones, rollingStones []Stones, fixedStones []Stones) []Stones {
-	wall := 0
-	for _, s := range rollingStones {
-		for _, f := range fixedStones {
-			if s.x > f.x {
-				wall = max(wall, f.x+1)
+func MoveNorth(stones []Stones, walls []Stones) {
+	stop := 0
+	y := 0
+	SortStones(stones, false, true) // Y then X ascending
+	for i, stone := range stones {
+		if stone.y != y { // change of column
+			stop = 0
+			y = stone.y
+		}
+		for _, wall := range walls {
+			if stone.y < wall.y {
+				break
+			} else if stone.y == wall.y {
+				if stone.x > wall.x {
+					stop = max(stop, wall.x+1)
+				}
 			}
 		}
-		tiltedStones = append(tiltedStones, Stones{x: wall, y: s.y})
-		wall++
+		stones[i].x = stop
+		stop++
 	}
-	return tiltedStones
 }
 
-func TiltTable(puzzle Puzzle) Puzzle {
-	fixedStones := FindStones(puzzle, '#')
-	rollingStones := FindStones(puzzle, 'O')
-
-	tiltedStones := []Stones{}
-
-	for j := 0; j < puzzle.cols; j++ {
-		fixedStonesCol := FilterStones(fixedStones, false, j)
-		rollingStonesCol := FilterStones(rollingStones, false, j)
-		tiltedStones = MoveStones(tiltedStones, rollingStonesCol, fixedStonesCol)
+func MoveSouth(stones []Stones, walls []Stones, rows int) {
+	stop := rows - 1
+	y := 0
+	SortStones(stones, false, false) // Y then X descending
+	for i, stone := range stones {
+		if stone.y != y { // change of column
+			stop = rows - 1
+			y = stone.y
+		}
+		for _, wall := range walls {
+			if stone.y < wall.y { // no more walls
+				break
+			} else if stone.y == wall.y {
+				if stone.x < wall.x {
+					stop = min(stop, wall.x-1)
+				}
+			}
+		}
+		stones[i].x = stop
+		stop--
 	}
-
-	newPuzzle := Puzzle{
-		rows: puzzle.rows,
-		cols: puzzle.cols,
-		load: 0,
-		grid: make([][]byte, puzzle.cols),
-	}
-	for i := range newPuzzle.grid {
-		newPuzzle.grid[i] = make([]byte, puzzle.rows)
-	}
-
-	for _, s := range fixedStones {
-		newPuzzle.grid[s.x][s.y] = '#'
-	}
-
-	for _, s := range tiltedStones {
-		newPuzzle.load += puzzle.rows - s.x
-		newPuzzle.grid[s.x][s.y] = 'O'
-	}
-
-	return newPuzzle
 }
 
-func PrintTable(puzzle Puzzle) {
-	for _, r := range puzzle.grid {
+func MoveWest(stones []Stones, walls []Stones) {
+	stop := 0
+	x := 0
+	SortStones(stones, true, true) // X then Y ascending
+	for i, stone := range stones {
+		if stone.x != x { // change of row
+			stop = 0
+			x = stone.x
+		}
+		for _, wall := range walls {
+			if stone.x < wall.x { // no more walls
+				break
+			} else if stone.x == wall.x {
+				if stone.y > wall.y {
+					stop = max(stop, wall.y+1)
+				}
+			}
+		}
+		stones[i].y = stop
+		stop++
+	}
+}
+
+func MoveEast(stones []Stones, walls []Stones, cols int) {
+	stop := cols - 1
+	x := 0
+	SortStones(stones, true, false) // X then Y descending
+	for i, stone := range stones {
+		if stone.x != x { // change of row
+			stop = cols - 1
+			x = stone.x
+		}
+		for _, wall := range walls {
+			if stone.x < wall.x { // no more walls
+				break
+			} else if stone.x == wall.x {
+				if stone.y < wall.y {
+					stop = min(stop, wall.y-1)
+				}
+			}
+		}
+		stones[i].y = stop
+		stop--
+	}
+}
+
+func CleanGrid(grid [][]byte) {
+	for _, r := range grid {
+		for i := 0; i < len(r); i++ {
+			if r[i] == 'O' {
+				r[i] = '.'
+			}
+		}
+	}
+}
+
+func PlaceStones(grid [][]byte, stones []Stones) [][]byte {
+	rows := len(grid)
+	newGrid := [][]byte{}
+	if rows > 0 {
+		newGrid = make([][]byte, rows)
+		for i, r := range grid {
+			newGrid[i] = slices.Clone(r)
+		}
+
+		for _, s := range stones {
+			newGrid[s.x][s.y] = 'O'
+		}
+	}
+	return newGrid
+}
+
+func PrintTable(grid [][]byte) {
+	for _, r := range grid {
 		for _, c := range r {
 			if c == 0 {
 				fmt.Print(".")
@@ -155,22 +285,113 @@ func PrintTable(puzzle Puzzle) {
 	fmt.Println()
 }
 
+func GetLoadNorth(stones *[]Stones, puzzle *Puzzle) int {
+	load := 0
+	for _, s := range *stones {
+		load += puzzle.rows - s.x
+	}
+	return load
+}
+
+func AutoCorrelation(timeseries []int) (int, float64) {
+	n := len(timeseries)
+	k := n / 2
+	max := 0.0
+	delay := 0
+	mean := 0.0
+
+	for i := 0; i < n; i++ {
+		mean += float64(timeseries[i])
+	}
+	mean /= float64(n)
+
+	for j := 0; j < k; j++ {
+		a := 0.0
+		for i := j; i < n-j; i++ {
+			a += (float64(timeseries[i]) - mean) * (float64(timeseries[i+j]) - mean)
+		}
+		if j > 5 && a > max {
+			max = a
+			delay = j
+		}
+	}
+	return delay, max
+}
+
+func InitPuzzle(puzzle *Puzzle) {
+	puzzle.emptyGrid = make([][]byte, len(puzzle.grid))
+	for i, r := range puzzle.grid {
+		puzzle.emptyGrid[i] = slices.Clone(r)
+	}
+	CleanGrid(puzzle.emptyGrid)
+
+	puzzle.fixedNorth = FindStones(puzzle.emptyGrid, '#')
+	SortStones(puzzle.fixedNorth, false, true) // Y then X ascending
+	puzzle.fixedWest = slices.Clone(puzzle.fixedNorth)
+	SortStones(puzzle.fixedWest, true, true) // X then Y ascending
+	puzzle.fixedSouth = slices.Clone(puzzle.fixedNorth)
+	SortStones(puzzle.fixedSouth, false, false) // Y then X descending
+	puzzle.fixedEast = slices.Clone(puzzle.fixedNorth)
+	SortStones(puzzle.fixedEast, true, false) // X then Y descending
+}
+
 func Solver1(puzzle Puzzle) int {
+	InitPuzzle(&puzzle)
+	stones := FindStones(puzzle.grid, 'O')
 
-	p := TiltTable(puzzle)
+	MoveNorth(stones, puzzle.fixedNorth)
 
-	return p.load
+	return GetLoadNorth(&stones, &puzzle)
 }
 
 func Solver2(puzzle Puzzle, cycles int) int {
+	InitPuzzle(&puzzle)
+	stones := FindStones(puzzle.grid, 'O')
+
+	load := 0
+	period := 0
+	n := 1000
+	history := make([]int, n)
+	prediction := make([]bool, n)
 
 	for i := 0; i < cycles; i++ {
-		// cycle
-		for j := 0; j < 4; j++ {
-			puzzle = TiltTable(puzzle)
-			puzzle = Rotate(puzzle, true)
+
+		// check our predictions
+		ok := true
+		for _, p := range prediction {
+			ok = ok && p
+		}
+
+		if ok {
+			j := (i - period) % n
+			k := (cycles - i - 1) % period
+			if j+period > n {
+				periodicSlice := history[j-period : j]
+				load = periodicSlice[k]
+			} else {
+				periodicSlice := history[j : j+period]
+				load = periodicSlice[k]
+			}
+			break
+		} else {
+			MoveNorth(stones, puzzle.fixedNorth)
+			MoveWest(stones, puzzle.fixedWest)
+			MoveSouth(stones, puzzle.fixedSouth, puzzle.rows)
+			MoveEast(stones, puzzle.fixedEast, puzzle.cols)
+
+			// calculate load and store in history array
+			load = GetLoadNorth(&stones, &puzzle)
+			history[i%n] = load
+
+			if i > n {
+				// look for repeating pattern
+				period, _ = AutoCorrelation(history)
+				if period > 0 {
+					prediction[i%n] = load == history[(i-period)%n]
+				}
+			}
 		}
 	}
 
-	return puzzle.load
+	return load
 }
--- 2023/day14/solver_test.go
+++ 2023/day14/solver_test.go
@@ -8,20 +8,20 @@ func TestSolver1(t *testing.T) {
 
 	puzzle := LoadPuzzle("test.txt")
 
-	test := func(puzzle Puzzle, expected int) {
-		result := Solver1(puzzle)
+	test := func(p Puzzle, expected int) {
+		result := Solver1(p)
 		if result != expected {
 			t.Errorf("Expected %d, result %d", expected, result)
 		}
 	}
 
-	test(Puzzle{}, 0)
+	//test(Puzzle{}, 0)
 	test(puzzle, 136)
 }
 
 func TestSolver2(t *testing.T) {
 
-	puzzle := LoadPuzzle("test2.txt")
+	puzzle := LoadPuzzle("test.txt")
 
 	test := func(puzzle Puzzle, cycles int, expected int) {
 		result := Solver2(puzzle, cycles)
@@ -31,8 +31,13 @@ func TestSolver2(t *testing.T) {
 	}
 
 	test(Puzzle{}, 0, 0)
-	test(puzzle, 1, 115)
-	test(puzzle, 2, 121)
-	test(puzzle, 3, 127)
-	test(puzzle, 1000000, 118)
+	test(puzzle, 1, 87)
+	test(puzzle, 2, 69)
+	test(puzzle, 3, 69)
+	test(puzzle, 100, 68)
+	test(puzzle, 1000, 64)
+	test(puzzle, 10000, 69)
+	test(puzzle, 100000, 65)
+	test(puzzle, 1000000, 63)
+	test(puzzle, 10000000, 69)
 }
